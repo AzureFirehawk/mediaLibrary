695de640258134a55845df81aaebeb89b5acb0ad
AzureFirehawk <madison.thomas094@gmail.com>
2026-02-05 16:43:49 -0600

    django app initial init

---
+++ mediaLibrary/media_library/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mediaLibrary project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mediaLibrary.settings')
+
+application = get_asgi_application()
