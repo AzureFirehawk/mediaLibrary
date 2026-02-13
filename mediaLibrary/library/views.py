3d9e95bbcfe75c3e3a623dc0c0ae9e3311533371
AzureFirehawk <madison.thomas094@gmail.com>
2026-02-13 11:25:59 -0600

    updates

--- mediaLibrary/library/views.py
+++ mediaLibrary/library/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import MediaItem
+from .forms import MediaItemForm
 
-# Create your views here.
+def media_list(request):
+    items = MediaItem.objects.all().order_by('-created_at')
+    return render(request, 'library/media_list.html', {
+        'items': items
+    })
+
+def media_detail(request, pk):
+    item = get_object_or_404(MediaItem, pk=pk)
+    return render(request, 'library/media_detail.html', {
+        'item': item
+    })
+
+def media_create(request):
+    if request.method == 'POST':
+        form = MediaItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('library:media_list')
+    else:
+        form = MediaItemForm()
+
+    return render(request, 'library/media_form.html', {
+        'form': form
+    })
